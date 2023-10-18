8901643ba4799144c100b5ef9656e0c1eccda967
zhenyamorozov <zhenya.morozov@gmail.com>
2023-10-18 15:55:13 +0000

    commited by IOS XE Automation

--- backup.py
+++ backup.py
@@ -2,8 +2,8 @@
 import os
 import cli
 output = cli.cli("show run")
-os.chdir("enauto_backup/")
-with open("show_run.conf", "w") as f:
+#os.chdir("enauto_backup/")
+with open("enauto_backup/show_run.conf", "w") as f:
     f.write(output)
 os.system("git add .")
 os.system("git commit -am 'commited by IOS XE Automation'")
